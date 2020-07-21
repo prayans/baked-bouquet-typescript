60999dfea430af731ba64d5e251907341d32dec1
prayans <tyagipriyanshu077@gmail.com>
2020-07-21 17:57:05 +0530

    practice

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
 import React from "react";
-import { ProminentAppBar } from "./Components/AppBar";
 import { NevBar } from "./Components/NevBar";
 import "./App.css";
 
 function App() {
-  const [mobileOpen, setMobileOpen] = React.useState(false);
-
-  const handleDrawerToggle = () => {
-    setMobileOpen(!mobileOpen);
-  };
   return <NevBar />;
 }
 
